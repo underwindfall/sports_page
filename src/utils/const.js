35315486b9508b43dfeb78cea58425e2b5d1c485
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-10-10 22:17:21 +0800

    cherry-pick upstream

--- src/utils/const.js
+++ src/utils/const.js
@@ -8,7 +8,6 @@ const MUNICIPALITY_CITIES_ARR = [
   '重庆市',
   '香港特别行政区',
   '澳门特别行政区',
-  '巴黎市',
 ];
 
 // styling: set to `true` if you want dash-line route
@@ -19,7 +18,7 @@ const LINE_OPACITY = 0.4;
 const MAP_HEIGHT = 600;
 
 // IF you outside China please make sure IS_CHINESE = false
-const IS_CHINESE = false;
+const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength, year) =>
   `户外运动 ${yearLength} 年 ` + ( year === 'Total' ? '' : `，地图展示的是 ${year} 年的轨迹`);
